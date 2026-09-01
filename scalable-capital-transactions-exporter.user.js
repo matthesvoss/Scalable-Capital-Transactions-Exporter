6b6d1a7aa3fc260228993b61f1d369d3e8236843
Matthes Voß <matthesvoss7@gmail.com>
2026-09-01 06:25:56 +0200

    bump version

--- scalable-capital-transactions-exporter.user.js
+++ scalable-capital-transactions-exporter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Scalable Capital Transactions Exporter
 // @namespace    http://tampermonkey.net/
-// @version      2026-05-29
+// @version      2026-09-01
 // @description  Export your Scalable Capital Transactions as a .csv (comma-separated values) file in German or English, ready to be imported into Portfolio Performance.
 // @author       Matthes Voß
 // @match        https://*.scalable.capital/broker/transactions*
